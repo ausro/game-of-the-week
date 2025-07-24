1617f1f57147b7e9c66603d8eab92c5159846326
ausro <216354256+ausro@users.noreply.github.com>
2025-07-24 13:50:01 -0600

    Add about

---
+++ frontend/src/modules/About.tsx
@@ -0,0 +1,21 @@
+export default function About() {
+    return (
+        <div id="about">
+            <h1>About</h1>
+            <h2>(Party) Game of the Week</h2>
+
+            <p>Have you ever had a game that all your friends were playing for a week, but then never heard of it again?<br/>Many people have names for these kinds of games, such as "Game of the Week", and go through many such games.</p>
+
+            <h2>What is a "Game of the Week"?</h2>
+            <ul>
+                <li>Cheap or free</li>
+                <li>Easy to jump into and out of</li>
+                <li>Not permanent progression-based</li>
+                <li>Shorter game loop</li>
+            </ul>
+
+            <p className="important">Most importantly:</p>
+            <li><em>You play it with your friends</em></li>
+        </div>
+    )
+}
--- frontend/src/routes/AppRoutes.tsx
+++ frontend/src/routes/AppRoutes.tsx
@@ -2,12 +2,14 @@ import { lazy } from "react"
 import {Route, Routes} from "react-router-dom"
 
 const SteamAppList = lazy(() => import("../modules/SteamAppList"))
+const About = lazy(() => import("../modules/About"))
 
 const AppRoutes = () => {
     return (
         <Routes>
             <Route path="/" element={<SteamAppList recommended={false}/>}> </Route>
             <Route path="/recommended" element={<SteamAppList recommended={true}/>}></Route>
+            <Route path="/about" element={<About />}></Route>
         </Routes>
     )
 }
